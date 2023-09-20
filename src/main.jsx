7f87399579e785e26d8e76bfba64f58825ccc70d
sjq1412 <sarahdev.cod3@gmail.com>
2023-09-20 20:41:39 +0800

    implementing adding note and toggling notes importance

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,58 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-// import './index.css'
+import { createStore } from 'redux'
+import noteReducer from './reducers/noteReducer'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
+const store = createStore(noteReducer)
+
+console.log(store.getState())
+
+const App = () => {
+  const addNote = event => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch({
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        id: generateId(),
+        important: false
+      }
+    })
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      payload: {
+        id
+      }
+    })
+  }
+
+  return (
+    <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {store.getState().map(note => <li key={note.id} onClick={() => toggleImportance(note.id)} >
+          {note.content} <strong>{note.important ? 'important': ''}</strong>
+        </li>)}
+      </ul>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
