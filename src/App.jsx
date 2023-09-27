c74dc0ff8e29d9fdf9fdc06d6636a4a8532f743f
sjq1412 <sarahdev.cod3@gmail.com>
2023-09-27 21:40:01 +0800

    add radio buttons for selecting importance

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,17 @@ import Notes from './components/Notes'
 import NewNote from './components/NewNote'
 
 const App = () => { 
+    const filterSelected = value => {
+        console.log(value)
+    }
     return (
         <div>
             <NewNote />
+            <div>
+                all <input type="radio" name="filter" onChange={() => filterSelected("ALL")} />
+                important <input type="radio" name="filter" onChange={() => filterSelected("IMPORTANT")} />
+                nonimportant <input type="radio" name="filter" onChange={() => filterSelected("NONIMPORTANT")} />
+            </div>
             <Notes />
         </div>
     )
